679e3501ed152f6a778e3dd9cee8cc8580705608
Brian Folts <bfolts@godaddy.com>
2012-09-04 09:27:53 -0700

    SchemaResource and ItemResource added.

---
+++ src/gdapi/ItemResource.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gdapi
+{
+    class ItemResource : Resource
+    {
+
+        public Dictionary<string, string> actions { get; set; }
+
+        /// <summary>
+        /// Constructs a new Resource
+        /// </summary>
+        public ItemResource()
+        {
+            this.actions = new Dictionary<string, string>();
+        }
+
+        /// <summary>
+        /// Creates a new Resource of a specific type. This constructor is usually used for creating a Resource to use for saving or deleting a Resource.
+        /// </summary>
+        /// <param name="type">Resource type</param>
+        public ItemResource(string type)
+        {
+            this.actions = new Dictionary<string, string>();
+            this.type = type;
+        }
+
+        /// <summary>
+        /// Determines if a Resource can currently perform an action.
+        /// </summary>
+        /// <param name="name">Action to perform</param>
+        /// <returns>True if the action exists in the actions dictionary, false otherwise</returns>
+        public bool hasAction(string name)
+        {
+            return this.actions.ContainsKey(name);
+        }
+
+        /// <summary>
+        /// Gets the current action url if it exists.
+        /// </summary>
+        /// <param name="name">Action to perform</param>
+        /// <returns>The current action url or null if action not found</returns>
+        public string getAction(string name)
+        {
+            return hasAction(name) ? this.actions[name] : null;
+        }
+
+    }
+
+}
---
+++ src/gdapi/SchemaResource.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Newtonsoft.Json.Linq;
+
+namespace gdapi
+{
+    public class SchemaResource : Resource
+    {
+
+        public Dictionary<string, Dictionary<string, string>> actions { get; set; }
+        public string[] collectionMethods { get; set; }
+        public Dictionary<string, string> collectionActions { get; set; }
+        public Dictionary<string, Dictionary<string,JToken>> collectionFilters { get; set;}
+        public Dictionary<string, Dictionary<string, JToken>> fields { get; set; }
+        public string[] methods { get; set; }
+
+
+        /// <summary>
+        /// Constructs a new Resource
+        /// </summary>
+        public SchemaResource()
+        {
+            this.actions = new Dictionary<string, Dictionary<string,string>>();
+        }
+
+        /// <summary>
+        /// Determines if a Resource can currently perform an action.
+        /// </summary>
+        /// <param name="name">Action to perform</param>
+        /// <returns>True if the action exists in the actions dictionary, false otherwise</returns>
+        public bool hasAction(string name)
+        {
+            return this.actions.ContainsKey(name);
+        }
+
+        /// <summary>
+        /// Gets the current action url if it exists.
+        /// </summary>
+        /// <param name="name">Action to perform</param>
+        /// <returns>The current action url or null if action not found</returns>
+        public Dictionary<string,string> getAction(string name)
+        {
+            return hasAction(name) ? this.actions[name] : null;
+        }
+
+    }
+}
